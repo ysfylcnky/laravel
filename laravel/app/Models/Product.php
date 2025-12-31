dc7a7efe0988911cd1beee688510122925fb774d
ysfylcnky <yusufyalcinkaya60@gmail.com>
2025-12-31 12:03:13 +0300

    define relationships between Category and Product models

--- laravel/app/Models/Product.php
+++ laravel/app/Models/Product.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
 
 class Product extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name', 'price', 'category_id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
