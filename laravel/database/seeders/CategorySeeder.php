6984d7499693463a44a5b9cd8785b2f798a0e962
ysfylcnky <yusufyalcinkaya60@gmail.com>
2025-12-31 12:14:41 +0300

    add category and product seeders and seed initial data

---
+++ laravel/database/seeders/CategorySeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Category::create(['name' => 'Elektronik']);
+        Category::create(['name' => 'Giyim']);
+    }
+}
---
+++ laravel/database/seeders/ProductSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Product::create([
+        'name' => 'Laptop',
+        'price' => 25000,
+        'category_id' => 1
+        ]);
+    }
+}
